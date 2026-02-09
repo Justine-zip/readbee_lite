e1932733c8cf037c4d9742eeabc96c9299f14485
Justine <justinecarl612@gmail.com>
2026-02-09 11:09:46 +0800

    added main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:readbee_lite/pages/main_page.dart';
 
 void main() => runApp(
   DevicePreview(enabled: !kReleaseMode, builder: (context) => MyApp()),
@@ -17,19 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-          child: Text(
-            'R E A D B E E',
-            style: TextStyle(
-              fontSize: 36,
-              fontWeight: FontWeight.bold,
-              color: Colors.amber,
-            ),
-          ),
-        ),
-      ),
+      home: const MainPage(),
     );
   }
 }
---
+++ lib/pages/main_page.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+
+        body: const TabBarView(
+          children: [
+            Center(
+              child: Text(
+                'H O M E',
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.amber,
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                'R E C O R D',
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.amber,
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                'M A T E R I A L',
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.amber,
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                'E V E N T',
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.amber,
+                ),
+              ),
+            ),
+            Center(
+              child: Text(
+                'P R O F I L E',
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.amber,
+                ),
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Material(
+            elevation: 3,
+            borderRadius: BorderRadius.circular(12),
+            child: const TabBar(
+              dividerColor: Colors.transparent,
+              labelColor: Colors.amber,
+              unselectedLabelColor: Colors.black38,
+              indicatorColor: Colors.white,
+              indicatorSize: TabBarIndicatorSize.label,
+              tabs: [
+                Tab(icon: Icon(Icons.home)),
+                Tab(icon: Icon(Icons.file_copy_rounded)),
+                Tab(icon: Icon(Icons.book_online_rounded)),
+                Tab(icon: Icon(Icons.calendar_month_rounded)),
+                Tab(icon: Icon(Icons.person_2_rounded)),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
