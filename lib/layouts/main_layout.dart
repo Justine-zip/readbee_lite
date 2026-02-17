c91c04af5ffb98c339dd742e5330adef05fe0177
Justine <justinecarl612@gmail.com>
2026-02-17 10:46:38 +0800

    implement tablet style in navigation bar

--- lib/layouts/main_layout.dart
+++ lib/layouts/main_layout.dart
@@ -101,36 +101,120 @@ class TabletMainLayout extends StatefulWidget {
 }
 
 class _TabletMainLayoutState extends State<TabletMainLayout> {
-  int selectedIndex = 0;
-
-  final pages = const [TabletReadingMaterialPage(), ProfilePage()];
-
   @override
   Widget build(BuildContext context) {
     debugPrint('Layout: Tablet');
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, result) {
+        showDialog(
+          context: context,
+          builder:
+              (context) => PromptBox(
+                title: 'Are you sure you want to Exit?',
+                onConfirm: () => debugPrint('Exited'),
+              ),
+        );
+      },
+      child: DefaultTabController(
+        length: 5,
+        child: Stack(
+          children: [
+            Scaffold(
+              backgroundColor: Theme.of(context).colorScheme.surface,
 
-    return Scaffold(
-      body: Row(
-        children: [
-          NavigationRail(
-            selectedIndex: selectedIndex,
-            onDestinationSelected:
-                (index) => setState(() => selectedIndex = index),
-            labelType: NavigationRailLabelType.all,
-            destinations: const [
-              NavigationRailDestination(
-                icon: Icon(Icons.book),
-                label: Text("Books"),
+              body: const TabBarView(
+                children: [
+                  Center(
+                    child: Text(
+                      'H O M E',
+                      style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.amber,
+                      ),
+                    ),
+                  ),
+                  RecordPage(),
+                  TabletReadingMaterialPage(),
+                  Center(
+                    child: Text(
+                      'E V E N T',
+                      style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.amber,
+                      ),
+                    ),
+                  ),
+                  ProfilePage(),
+                ],
               ),
-              NavigationRailDestination(
-                icon: Icon(Icons.person),
-                label: Text("Profile"),
+            ),
+            Positioned(
+              //top: 30,
+              bottom: 30,
+              left: 250,
+              right: 250,
+              child: Material(
+                color: Theme.of(context).colorScheme.surfaceContainer,
+                elevation: 3,
+                borderRadius: BorderRadius.circular(12),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: TabBar(
+                    dividerColor: Colors.transparent,
+                    labelColor: Colors.amber,
+                    unselectedLabelColor:
+                        Theme.of(context).colorScheme.secondary,
+                    indicatorColor: Theme.of(context).colorScheme.surface,
+                    indicatorSize: TabBarIndicatorSize.label,
+                    tabs: [
+                      Tab(icon: Icon(Icons.home)),
+                      Tab(icon: Icon(Icons.file_copy_rounded)),
+                      Tab(icon: Icon(Icons.book_online_rounded)),
+                      Tab(icon: Icon(Icons.calendar_month_rounded)),
+                      Tab(icon: Icon(Icons.person_2_rounded)),
+                    ],
+                  ),
+                ),
               ),
-            ],
-          ),
-          Expanded(child: pages[selectedIndex]),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
+  // int selectedIndex = 0;
+
+  // final pages = const [TabletReadingMaterialPage(), ProfilePage()];
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   debugPrint('Layout: Tablet');
+
+  //   return Scaffold(
+  //     body: Row(
+  //       children: [
+  //         NavigationRail(
+  //           selectedIndex: selectedIndex,
+  //           onDestinationSelected:
+  //               (index) => setState(() => selectedIndex = index),
+  //           labelType: NavigationRailLabelType.all,
+  //           destinations: const [
+  //             NavigationRailDestination(
+  //               icon: Icon(Icons.book),
+  //               label: Text("Books"),
+  //             ),
+  //             NavigationRailDestination(
+  //               icon: Icon(Icons.person),
+  //               label: Text("Profile"),
+  //             ),
+  //           ],
+  //         ),
+  //         Expanded(child: pages[selectedIndex]),
+  //       ],
+  //     ),
+  //   );
+  // }
 }
