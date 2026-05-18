9faa52a6c305037ff5d014ca96c6396aedf03042
Justine <justinecarl612@gmail.com>
2026-05-18 23:10:42 +0800

    add calendar provider and custom style

--- lib/pages/event/event_page.dart
+++ lib/pages/event/event_page.dart
@@ -1,23 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:readbee_lite/providers/calendar_event_provider.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class EventPage extends StatelessWidget {
+class EventPage extends ConsumerStatefulWidget {
   const EventPage({super.key});
 
   @override
+  ConsumerState<EventPage> createState() => _EventPageState();
+}
+
+class _EventPageState extends ConsumerState<EventPage> {
+  @override
   Widget build(BuildContext context) {
+    final daySelector = ref.watch(selectedDay);
+
     return Scaffold(
       body: Column(
         children: [
-          SizedBox(
-            width: MediaQuery.of(context).size.width * .8,
-            child: TableCalendar(
-              firstDay: DateTime.utc(2010, 10, 16),
-              lastDay: DateTime.utc(2030, 3, 14),
-              focusedDay: DateTime.now(),
+          SizedBox(height: 100),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.surfaceContainer,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              width: MediaQuery.of(context).size.width * .8,
+              child: TableCalendar(
+                calendarStyle: CalendarStyle(
+                  defaultDecoration: BoxDecoration(color: Colors.amber[300]),
+                  defaultTextStyle: TextStyle(
+                    color: Theme.of(context).colorScheme.surfaceVariant,
+                  ),
+                ),
+                selectedDayPredicate: (day) {
+                  return isSameDay(daySelector, day);
+                },
+
+                onDaySelected: (pickedDay, focusedDay) {
+                  ref.read(selectedDay.notifier).state = pickedDay;
+                },
+
+                firstDay: DateTime.utc(2010, 10, 16),
+                lastDay: DateTime.utc(2030, 3, 14),
+                focusedDay: daySelector,
+              ),
             ),
           ),
-          Center(
+
+          const Center(
             child: Text(
               'E V E N T - X',
               style: TextStyle(
