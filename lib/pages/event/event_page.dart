3e83ee2d527aaf1a1408b139a2a833ca38ba9c40
Justine <justinecarl612@gmail.com>
2026-05-18 17:18:38 +0800

    added table_calendar

--- lib/pages/event/event_page.dart
+++ lib/pages/event/event_page.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class EventPage extends StatelessWidget {
   const EventPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'E V E N T - X',
-          style: TextStyle(
-            fontSize: 36,
-            fontWeight: FontWeight.bold,
-            color: Colors.amber,
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width * .8,
+            child: TableCalendar(
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              focusedDay: DateTime.now(),
+            ),
           ),
-        ),
+          Center(
+            child: Text(
+              'E V E N T - X',
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+                color: Colors.amber,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
