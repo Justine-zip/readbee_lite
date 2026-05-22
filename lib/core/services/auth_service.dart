58ef1cf819b63890ecb16345d617e03aad367d33
Justine <justinecarl612@gmail.com>
2026-05-22 15:07:24 +0800

    connect supabase to readbee

---
+++ lib/core/services/auth_service.dart
@@ -0,0 +1,73 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class AuthServices {
+  final SupabaseClient _supabase = Supabase.instance.client;
+
+  Future<AuthResponse> signInWithEmailPassword(
+    String email,
+    String password,
+  ) async {
+    return await _supabase.auth.signInWithPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<AuthResponse> signUpWithEmailPassword(
+    String email,
+    String password,
+    String name,
+  ) async {
+    final response = await _supabase.auth.signUp(
+      email: email,
+      password: password,
+    );
+
+    final userId = response.user?.id;
+    if (userId != null) {
+      await _supabase.from('users').insert({
+        'user_id': userId,
+        'email': email,
+        'fname': name,
+      });
+    }
+
+    return response;
+  }
+
+  Future<void> signOut() async {
+    await _supabase.auth.signOut();
+  }
+
+  String? getCurrentUserId() {
+    return _supabase.auth.currentUser?.id;
+  }
+
+  String? getCurrentUserEmail() {
+    final session = _supabase.auth.currentSession;
+    final user = session?.user;
+    return user?.email;
+  }
+
+  Future<bool> resetCurrentUserPassword(String newPassword) async {
+    try {
+      final user = await _supabase.auth.updateUser(
+        UserAttributes(password: newPassword),
+      );
+      print('Password updated for: ${user.user!.email}');
+      return true;
+    } catch (e) {
+      print('Failed to update password: $e');
+      return false;
+    }
+  }
+
+  Future<void> sendPasswordResetEmail(String email) async {
+    try {
+      await _supabase.auth.resetPasswordForEmail(email);
+      print('Password reset email sent to $email');
+    } catch (e) {
+      print('Failed to send reset email: $e');
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,22 @@ import 'package:readbee_lite/layouts/orientation_manager.dart';
 import 'package:readbee_lite/pages/main_page.dart';
 import 'package:readbee_lite/providers/theme_provider.dart';
 import 'package:readbee_lite/themes/app_theme.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() => runApp(
-  DevicePreview(
-    enabled: !kReleaseMode,
-    builder: (context) => ProviderScope(child: MyApp()),
-  ),
-);
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://vwpcsfuytvymxhahfmun.supabase.co',
+    anonKey: 'sb_publishable_iLDvUdz4hQoqrZq3GKKlaQ_4CHlxAhX',
+  );
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => ProviderScope(child: MyApp()),
+    ),
+  );
+}
 
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
