7b69a53e735a8268fb3ac9017469b82633f7eb25
Carl Justine <justinecarl612@gmail.com>
2026-06-09 12:51:20 +0800

    initialze ci test init

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:readbee_lite/main.dart';
 
 void main() {
@@ -16,7 +15,7 @@ void main() {
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
+    expect(find.text('10'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
